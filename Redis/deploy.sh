f809af3d3283295f30988da1272f6d583c374fa4
Carlos278 <55208451+Carlos278@users.noreply.github.com>
2021-12-28 13:13:31 -0300

    Update deploy.sh

--- Redis/deploy.sh
+++ Redis/deploy.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+#Instalar o helm
+helm install
+#add repo r-
+helm repo add bitnami https://charts.bitnami.com/bitnami
+#upgrade de repo redis
+helm upgrade redis bitnami/redis --install --namespace=kubernetes-intro --values=/redis/values.yaml
+#upgrade de repo guestbook
+helm upgrade guestbook ./guestbook-config --install --namespace=kubernetes-intro --values=/redis/values.yaml
 #Verifique a existência de um chart helm instalado
 helm ls
 #Verifique a quais repositórios você está conectado
